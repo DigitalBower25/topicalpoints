62032a51ae07e2bd1f228acbf30d463cb5d20859
DigitalBower25 <hello@digitalbower.com>
2025-01-23 13:36:44 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,9 +3,9 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 $servername = "127.0.0.1:3306";
-$username = "u169755773_root";
-$password = "Admin_blog@0210";
-$dbname = "u169755773_blog";
+$username = "xxxxxxxx";
+$password = "xxxxxxxx";
+$dbname = "xxxxxxxxx";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
